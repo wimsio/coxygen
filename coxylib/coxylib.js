0928a34e3a1f98c9d507161acd0a63231a0ee520
Bernard Sibanda <dev@wims.io>
2024-07-19 18:02:45 +0200

    Update coxylib.js

--- coxylib/coxylib.js
+++ coxylib/coxylib.js
@@ -1,3 +1,22 @@
+/*
+Version : 1.0.1
+Coxylib is a set of atomic functions simplifying use of Helios smart contract library.
+Author:         Bernard Sibanda (Coxygen Global Pty Ltd)
+License :       MIT License
+Installation :  Import this coxylib.js file to any project even static websites. Please note there are Helios and Jimba dependecies
+Date Started:   2024
+
+Advantages:
+- it gives testable atomic funtions for Cardano blockchain integration
+- simplifies and speeds up decentralized cardano development
+- easy deployment even on cpanel websites
+- comes packed with testing library jimba.js and also standalone Helios.js
+- uses a much improved console.logs which can be switched on and off
+- 100% client side dapp development
+- no need for npm, nodejs, and other painful bloating packages
+- implements code best design functions e.g. code re-use, functional programming, etc 
+*/
+
 import {
 			bytesToHex,Cip30Wallet,WalletHelper,TxOutput,
 			Assets,bytesToText,hexToBytes,AssetClass,
@@ -5,6 +24,8 @@ import {
 		} from "./js/helios.js";
 		
 import {opt,j} from "./js/jimba.js";
+
+import  {mintAssetsScript} from './mintAssetsScript.js';
 		
 export const hlib = {
     Value:Value,
@@ -26,6 +47,78 @@ export const txPrerequisites = {
 	minChangeAmt : 2000000, 
 	networkParamsUrl : "https://d1t0d7c2nekuk0.cloudfront.net/preprod.json",
 	minAda : 4000000	
+}
+
+const mintAssets = async (walletData,name,description,imageUrl,txPrerequisites,j,hlib,txFunc,mintAssetsScript) => 
+  {
+      try
+      {
+      
+  	    j.s("mintCNFT");
+		j.log({walletData});
+		j.log({txPrerequisites});
+		j.log({mintAssetsScript});
+		
+		const maxTxFee = txPrerequisites.maxTxFee; 
+		const minChangeAmt = txPrerequisites.minChangeAmt; 
+		const minAda = txPrerequisites.minAda;	
+		const minUTXOVal = new hlib.Value(BigInt(minAda + maxTxFee + minChangeAmt));	
+		const txIdHex = walletData.utxos[0][0].txId; j.log({txIdHex});
+		const utxoIdx = walletData.utxos[0][0].utxoIdx; j.log({utxoIdx})
+		
+
+		const mintScript = mintAssetsScript(txIdHex,utxoIdx,name).toString(); j.log({mintScript})
+
+		const nftCompiledProgram = hlib.Program.new(mintScript).compile(true); j.log({nftCompiledProgram})
+		
+		const tx = new hlib.Tx();
+		tx.addInputs(walletData.utxos[0]);
+		const nftMPH = nftCompiledProgram.mintingPolicyHash;
+		tx.attachScript(nftCompiledProgram);
+		const nftTokenName = hlib.ByteArrayData.fromString(name).toHex();
+		const nft = [[hlib.hexToBytes(nftTokenName), BigInt(1)]];
+		const mintRedeemer = new hlib.ConstrData(0, []);
+		tx.mintTokens(nftCompiledProgram.mintingPolicyHash,nft,mintRedeemer);
+		const toAddress = (await walletData.walletHelper.baseAddress).toBech32(); j.log({toAddress})
+		tx.addOutput(new hlib.TxOutput(hlib.Address.fromBech32(toAddress), new hlib.Value(minUTXOVal.lovelace, new hlib.Assets([[nftCompiledProgram.mintingPolicyHash, nft]]))));
+		tx.addMetadata(721, {"map": [[nftCompiledProgram.mintingPolicyHash.hex, {"map": [[name,
+										{
+											"map": [["name", name],
+													["description", description],
+													["image", imageUrl]
+												]
+										}
+									]]}
+									]]
+							}
+					);
+
+ 	    const txh = txFunc(walletData,hlib,tx,j,txPrerequisites.networkParamsUrl);
+		j.s("mintCNFT");
+      } catch (error) {
+		const errorMsg = await error.info;
+		 j.log({errorMsg});
+	}
+}
+
+export const mint = async (tokenName,tokenDescription,tokenImageUrl)=>{ 
+const wallet =  await init(j);
+j.test("coxylib testPack", "init",wallet).eq("lace")
+
+const walletData = await walletEssentials(wallet,Cip30Wallet,WalletHelper,Value,txPrerequisites.minAda,j); j.log({walletData})
+j.test("coxylib testPack","walletEssentials walletEnabled",walletData.walletEnabled).notBool() 
+
+ mintAssets(
+        walletData,
+        tokenName,
+        tokenDescription,
+        tokenImageUrl,
+        txPrerequisites,
+        j,
+        hlib,
+        txFunc,
+        mintAssetsScript
+    )
 }
 
 export function hexToTex(hexx) {
@@ -311,3 +404,81 @@ export const assetFunc = async (walletData,AssetClass,j)=>{
     return assetArray;
 }
 
+//new functions
+
+export const deadLine =async (tx,networkParams,deadLineMinutes,j)=>{
+        const slot = networkParams.liveSlot;  j.log({slot});
+        const time = networkParams.slotToTime(slot);  j.log({time});
+        const now = new Date(Number(time));		 j.log({now});
+        const before = new Date(now.getTime());  
+        before.setMinutes(now.getMinutes() - 5);  j.log({before});
+        const after = new Date(now.getTime()); 
+        after.setMinutes(now.getMinutes() + deadLineMinutes);  j.log({after});
+        tx.validFrom(before);  j.log({tx});
+        tx.validTo(after);		 j.log({tx});
+		return tx;
+}
+
+export const addSigner = async (networkParams,partyAddress,partyAutxos,tx,j,signersPKHs,owner="")=>{
+  j.log({signersPKHs});	
+  for(let i = 0; i < signersPKHs.length; i++ )
+  {
+	 tx.addSigner(signersPKHs[i]);  
+  }	
+	await tx.finalize(networkParams, partyAddress, partyAutxos);	 j.log({tx});	
+	const partySignature = await partyAddress.signTx(tx);  j.log({partySignature});	
+	tx.addSignatures(partySignature);	 j.log({tx});	
+	if(owner != "")
+	{
+	  const ownerSignature = await owner.signTx(tx); j.log({ownerSignature});	
+	  tx.addSignatures(ownerSignature);  j.log({tx});	
+	}
+	else
+	{
+		const ownerSignature = await owner.signTx(tx);  j.log({ownerSignature});	
+		tx.addSignatures(ownerSignature);	 j.log({tx});	
+	}		
+	return tx;	
+}
+
+export const submitTx =async(tx,network,j)=>{
+	const txId = await network.submitTx(tx);j.log({txId})
+	return txId;
+}
+
+export const attachScript =async(tx,script,j)=>{
+     await tx.attachScript(script); j.log({tx})	 
+	 return tx;
+}
+
+export const addTxOutPuts = async(tx,addresses,datums,j) => {		
+	for(let i = 0; i < addresses.length; i++ )
+	{
+	   await tx.addOutput(new TxOutput(addresses[i],datums[i]));
+	};		
+	j.log({tx});		  
+	return tx;
+}
+
+export const txInputs =async(tx,utxos,j)=>{
+ await tx.addInputs(utxos); j.log({tx})
+ return tx;
+}
+
+export const txInput =(tx,utxo,redeemer,j)=>{
+  tx.addInput(utxo, redeemer);  j.log({tx})
+  return tx;
+}
+
+export const mintTokens =async(tx,mph,token,redeemer,j)=>{
+       await tx.mintTokens(mph,token,redeemer); j.log({tx})		
+	return tx;
+}
+
+export const txRefInput =async(tx,utxo,j)=>{
+ await tx.addRefInput(utxo); j.log({tx})
+ return tx;
+}
+
+
+ 
