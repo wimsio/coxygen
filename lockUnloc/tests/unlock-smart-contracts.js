d8ce8c827327e029733748278dcc545fc3bf508f
Bernard Sibanda <dev@wims.io>
2024-10-11 10:30:21 +0200

    Update unlock-smart-contracts.js

--- lockUnloc/tests/unlock-smart-contracts.js
+++ lockUnloc/tests/unlock-smart-contracts.js
@@ -1,1 +1,14 @@
-//This is testing the unlock source code
+//This is a test code on the unlock smart contract
+
+//Output is displayed below Jimba speeds up front end testing and simplifies smart contract development 
+
+//source : Anyone can get the library here https://www.npmjs.com/package/jimba
+
+//link to live hosted demo : https://coxygen.co/coxygen.co/demo/tests/index.html
+
+ ```
+
+ ```
+
+
+
